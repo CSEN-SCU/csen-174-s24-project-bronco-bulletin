319428aa9cde8f7982e359748d931668fdba013b
Eric <eric.hicks.8989@gmail.com>
2024-04-28 13:14:18 -0700

    Create header bar for home UI.

---
+++ Frontend/bronco-bulletin/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/header.css";
+
+const Header = () => {
+	return (
+		<header>
+      <nav className="navbar shadow-sm p-3">
+        <a className="logo navbar-brand navbar mx-2" href="/">
+          Bronco Bulletin
+        </a>
+      </nav>
+		</header>
+	);
+};
+
+export default Header;
