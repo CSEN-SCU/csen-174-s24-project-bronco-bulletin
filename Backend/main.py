f52495f6c5ac64d6280b7398c86e5c52d2eb6980
James Vong <jvong@scu.edu>
2024-05-21 14:30:47 -0700

    Gunicorn, Procfile, port configuration

--- Backend/main.py
+++ Backend/main.py
@@ -86,4 +86,5 @@ def get_posts_by_tag(tag_name):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
