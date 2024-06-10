c41faa445c57b7a80f5ddcf206a65122b3305847
gorloff <93233372+gorloff@users.noreply.github.com>
2024-06-10 15:49:32 -0700

    Update Post.jsx
    added clickable and hover

--- Frontend/bronco-bulletin/src/pages/Home/Components/Post/Post.jsx
+++ Frontend/bronco-bulletin/src/pages/Home/Components/Post/Post.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./styles/post.css";
 
-const Post = ({banner, title, description}) => {
-	return (
-		<div className="item card shadow p-3 mt-3 bg-white rounded">
-      <img className="card-img-top img rounded" src={banner} alt="Card cap" />
-      <div className="card-body">
-        <h5 className="card-title h6 text-md">{ title }</h5>
-        <p className="card-text text-sm">{ description }</p>
+const Post = ({ banner, title, description }) => {
+  return (
+    <a href={banner} target="_blank" rel="noopener noreferrer" className="card-link">
+      <div className="item card shadow p-3 mt-3 bg-white rounded">
+        <img className="card-img-top img rounded" src={banner} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title h6 text-md">{title}</h5>
+          <p className="card-text text-sm">{description}</p>
+        </div>
       </div>
-    </div>
-	);
+    </a>
+  );
 };
 
 export default Post;
