5913d5874b0bb49605ea1f0e565bd03a7543de04
Eric <eric.hicks.8989@gmail.com>
2024-06-10 20:32:54 -0700

    Add margin bottom for posts.

--- Frontend/bronco-bulletin/src/pages/Home/Components/Post/Post.jsx
+++ Frontend/bronco-bulletin/src/pages/Home/Components/Post/Post.jsx
@@ -4,7 +4,7 @@ import "./styles/post.css";
 const Post = ({ banner, title, description }) => {
   return (
     <a href={banner} target="_blank" rel="noopener noreferrer" className="card-link">
-      <div className="item card shadow p-3 mt-3 bg-white rounded">
+      <div className="item card shadow p-3 mt-3 mb-3 bg-white rounded">
         <img className="card-img-top img rounded" src={banner} alt="Card cap" />
         <div className="card-body">
           <h5 className="card-title h6 text-md">{title}</h5>
